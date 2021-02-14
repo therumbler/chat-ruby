f48f1d4128d6dfac48dbe2f729667e804dcc5150
Benjamin Rumble <therumbler@gmail.com>
2021-02-14 15:26:35 +0000

    static file support

--- lib/app.rb
+++ lib/app.rb
@@ -4,25 +4,54 @@ require 'faye/websocket'
 
 require_relative './processor'
 
-def read_index
-  file = File.open('./static/index.html')
+def read_static(path)
+  path = path.gsub("/static/", "")
+  puts "DEBUG: path #{path}"
+  file = File.open("./static/#{path}")
   contents = file.read
   file.close
   contents
 end
 
+def process_static(env)
+  puts "INFO: REQUEST_PATH #{env['REQUEST_PATH']}"
+  content_type = 'text/html'
+  if env['REQUEST_PATH'].start_with? '/static'
+    if env['REQUEST_PATH'].end_with? '.js'
+      content_type = 'text/javascript;charset=UTF-8'
+    end
+    contents = read_static(env['REQUEST_PATH'])
+  end
+
+  if env['REQUEST_PATH'] == '/'
+    puts 'DEBUG: load index.html'
+    contents = read_static('/static/index.html')
+  end
+  return contents, content_type
+end
+
 App = lambda do |env|
   if Faye::WebSocket.websocket?(env)
     ws = Faye::WebSocket.new(env)
-    proc = Processor.new ws
-    Thread.new do |_task|
-      proc.background
-    end
+    Processor.new ws
+ 
     # Return async Rack response
     ws.rack_response
   else
     # Normal HTTP request
-    html = read_index
-    [200, { 'Content-Type' => 'text/html', 'Content-Length' => html.length.to_s }, [html]]
+    (contents, content_type) = process_static(env)
+    # html = read_index
+    if contents
+      [
+        200, 
+        { 
+          'Content-Type' => content_type, 
+          'Content-Length' => contents.length.to_s 
+        },
+        [contents]
+      ]
+    else
+      [400, {'content-type'=>'text/plain'}, ['not found']]
+    end
   end
 end
